5e4cff84be66742fa47046fd331a11b4ddd07bef
stephen Comino <scomino@gmail.com>
2022-05-18 12:01:33 +1000

    Added Log4Net to each project

---
+++ ServiceTicketClientApp/Model/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace Model
+{
+    public class Ticket
+    {
+        private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        //Test commit
+
+        public Ticket()
+        {
+        }
+    }
+}
