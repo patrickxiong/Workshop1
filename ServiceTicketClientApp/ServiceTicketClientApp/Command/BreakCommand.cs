e64818b400687643f8619822d73be2cd777fbaa6
stephen Comino <scomino@gmail.com>
2022-05-18 15:48:50 +1000

    Changes to commandes

--- ServiceTicketClientApp/ServiceTicketClientApp/Command/BreakCommand.cs
+++ ServiceTicketClientApp/ServiceTicketClientApp/Command/BreakCommand.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace ServiceTicketClientApp.Command
 {
-    public class BreakCommand : ICommand
+    public class BreakCommand : ICommand 
     {
         Action _execute;
         public event EventHandler CanExecuteChanged;
