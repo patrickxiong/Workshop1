9cdcd72a8245c603f40beff5b200e3deb7f2ac3f
stephen Comino <scomino@gmail.com>
2022-05-18 13:48:54 +1000

    Adding ViewModel Project and Files

---
+++ ServiceTicketClientApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class MainWindowViewModel
+    {
+        private string _userId;
+        private string _campaign;
+        private string _ticketId;
+        private string _type;
+
+    }
+}
