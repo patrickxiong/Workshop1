b9328f3c419683c90c9e58af3d328bb5195bd8b7
harrywz <wei.zhou@aristocrat.com>
2022-05-18 13:35:44 +1000

    Add initial changes

---
+++ ServiceTicketClientApp/Communication/NetworkConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Communication
+{
+    public class NetworkConfiguration
+    {
+        public string Server { get; set; }
+
+        public int Port { get; set; }
+
+        public string User { get; set; }
+
+        public string Password { get; set; }
+
+        public string Extension { get; set; }
+    }
+}
