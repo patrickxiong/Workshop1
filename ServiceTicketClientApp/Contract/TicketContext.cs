ee2a954f121dd05ab842122bb35d812d10342a0c
Patrick Xiong <patrick.xiong@aristocrat.com>
2022-05-18 12:57:51 +1000

    add unit test for contract

--- ServiceTicketClientApp/Contract/TicketContext.cs
+++ ServiceTicketClientApp/Contract/TicketContext.cs
@@ -15,10 +15,23 @@ namespace Contract
             //modelBuilder.Configurations.Add(new StudentConfigurations());
 
             modelBuilder.Entity<Tickets>()
+                .HasKey(t=>t.Ticket_ID)
                 .ToTable("Tickets");
 
-//            modelBuilder.Entity<Teacher>()
-//                .MapToStoredProcedures();
+            modelBuilder.Entity<Contacts>()
+                .HasKey(c => c.Contact_GUID)
+                .ToTable("Contacts");
+
+            modelBuilder.Entity<TicketTypes>()
+                .HasKey(t => t.TicketTypeCode)
+                .ToTable("TicketTypes");
+
+            modelBuilder.Entity<Departments>()
+                .HasKey(d => d.Department_ID)
+                .ToTable("Departments");
+
+            //            modelBuilder.Entity<Teacher>()
+            //                .MapToStoredProcedures();
         }
 
         public DbSet<Tickets> Tickets { get; set; }
---
+++ ServiceTicketClientApp/ContractTests/UnitOfWorkTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract.Tests
+{
+    [TestClass()]
+    public class UnitOfWorkTests
+    {
+        [TestMethod()]
+        public void UnitOfWorkTest()
+        {
+            var target = new UnitOfWork(new TicketContext());
+
+            var t=target.Tickets.GetByID(777);
+
+        }
+
+        [TestMethod()]
+        public void CommitTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
